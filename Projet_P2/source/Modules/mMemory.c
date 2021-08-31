3af631540bef2cd6ad3faed31813ba7ccc9e3790
Froidevaux Luc <luc.froideva@he-arc.ch>
2021-08-31 11:45:00 +0200

    Started working on memory module

---
+++ Projet_P2/source/Modules/mMemory.c
@@ -0,0 +1,42 @@
+/*
+ * mMemory.c
+ *
+ *  Created on: 31 août 2021
+ *      Author: luc.froideva
+ */
+
+
+#include "mMemory.h"
+
+#include "iI2C.h"
+#include "iDio.h"
+
+#define MEM_READ_BIT 0x01
+#define MEM_ADDR_BASE 0b10100000
+#define MEM_ADDR_A0 1
+#define MEM_ADDR_A1 1
+#define MEM_ADDR_A2 1
+
+void mMemory_Setup()
+	{
+	iI2C0_Config();
+
+	iDio_EnablePortClk();
+
+	iDio_PinConfig(kPortC, kPin12, kAlternate1); // MEM_A0
+	iDio_PinConfig(kPortC, kPin13, kAlternate1); // MEM_A1
+	iDio_PinConfig(kPortC, kPin16, kAlternate1); // MEM_A2
+	iDio_PinConfig(kPortC, kPin17, kAlternate1); // MEM_WP
+
+	iDio_SetPortDirection(kPortC, kMaskIo12, kIoOutput);
+	iDio_SetPortDirection(kPortC, kMaskIo13, kIoOutput);
+	iDio_SetPortDirection(kPortC, kMaskIo16, kIoOutput);
+	iDio_SetPortDirection(kPortC, kMaskIo17, kIoOutput);
+
+	iDio_SetPort(kPortC, kMaskIo12, MEM_ADDR_A0);
+	iDio_SetPort(kPortC, kMaskIo13, MEM_ADDR_A1);
+	iDio_SetPort(kPortC, kMaskIo16, MEM_ADDR_A2);
+
+	}
+
+
---
+++ Projet_P2/source/Modules/mMemory.h
@@ -0,0 +1,16 @@
+/*
+ * mMemory.h
+ *
+ *  Created on: 31 août 2021
+ *      Author: luc.froideva
+ */
+
+#ifndef MODULES_MMEMORY_H_
+#define MODULES_MMEMORY_H_
+
+void mMemory_Setup();
+
+void mMemory_Read(unsigned int address, unsigned int data);
+
+
+#endif /* MODULES_MMEMORY_H_ */
